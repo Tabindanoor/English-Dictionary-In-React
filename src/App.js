ad3723baaaf47a856811fd61c4be9c722a4189ff
TabindaNoor <tabindanoor415@gmail.com>
2024-03-19 19:39:11 +0500

    dictioanry

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import DiceGame from './DiceGame';
+import Result from './Result';
 
 function App() {
   return (
-    <div className="App">
+    <div className=" flex ">
+      <div className='flex text-center center' >
+        <div>
+        <p className='text-2xl '>English Dictionary</p>
+        <textarea name="" id="" cols="30" rows="10" className='border'/>
+        <select name="" id="">
+          <option value="">english</option>
+        </select>
+        
+
+
+        </div>
+      </div>
+      <Result/>
     </div>
   );
 }
---
+++ src/Result.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Result = () => {
+  return (
+    <div>
+        <p>Hello</p>
+        <p>/he 'leu / /he 'lou/</p>
+        <p>Noun</p>
+        <div>
+            <p>Meaning</p>
+            <p>hello or an equivalent greeting</p>
+        </div>
+        <div>
+            <p>Synonyms</p>
+            <p>greeting, greeting, greeting</p>
+        </div>
+    </div>
+  )
+}
+
+export default Result
